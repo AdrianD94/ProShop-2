7610cacc9a55e665ca09f1408c5083d724fc73c6
adrian <adrian.drozman@softvision.com>
2023-05-05 15:48:14 +0300

    finished homeScreen and productScreen

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <Outlet />
+      </Container>
+    </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
